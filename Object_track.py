e66cc96ca7844b59c6be41e030d487ea80d38a49
Satyam <satyamrsingh857@gmail.com>
2018-02-02 19:04:07 +0000

    Yello Object is tracking

--- Object_track.py
+++ Object_track.py
@@ -23,14 +23,23 @@ while(True):
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
-    
-    # Bitwise-AND mask and original image
-    res = cv2.bitwise_and(frame,frame, mask= mask)
-    
+
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[-2]
+    center = None
+
+    #check there is any Yello Object 
+    if len(cnts) > 0:
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))    
+        cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2)
+        cv2.circle(frame, center, 5, (0, 0, 255), -1)
+ 
+    # show the frame to our screen
     cv2.imshow('frame',frame)
     cv2.imshow('mask',mask)
-    #cv2.imshow('res',res)
-    #cv2.imshow('hvc',hsv)
+        
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
